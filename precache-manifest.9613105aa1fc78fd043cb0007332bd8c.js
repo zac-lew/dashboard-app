3641d2c7fd95be265bfed92259a4fc21dab8809d
zaclew922 <zaclew922@gmail.com>
2020-04-21 08:30:01 +1000

    Updates

---
+++ precache-manifest.9613105aa1fc78fd043cb0007332bd8c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d3016aed22fffdc6711647230dfb29",
+    "url": "/dashboard-app/index.html"
+  },
+  {
+    "revision": "2bb3dbe9409cda970e8e",
+    "url": "/dashboard-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "87cafbf17676209c389f",
+    "url": "/dashboard-app/static/css/main.9aa34aa4.chunk.css"
+  },
+  {
+    "revision": "2bb3dbe9409cda970e8e",
+    "url": "/dashboard-app/static/js/2.994cf1fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dashboard-app/static/js/2.994cf1fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87cafbf17676209c389f",
+    "url": "/dashboard-app/static/js/main.2ec0fe02.chunk.js"
+  },
+  {
+    "revision": "7f4a46bd89a841367e83",
+    "url": "/dashboard-app/static/js/runtime-main.ee257213.js"
+  },
+  {
+    "revision": "f3119f9477ecf5c4fdc7a6e4d0d2db31",
+    "url": "/dashboard-app/static/media/bed.f3119f94.svg"
+  },
+  {
+    "revision": "c8417a75c16027b8c7c5ec7e90bc26f6",
+    "url": "/dashboard-app/static/media/gymnast.c8417a75.svg"
+  },
+  {
+    "revision": "eeeca435e82a4f795027bcf6fb65e018",
+    "url": "/dashboard-app/static/media/loading.eeeca435.gif"
+  },
+  {
+    "revision": "4cf0ed281af00e69b00ffce886813d84",
+    "url": "/dashboard-app/static/media/physiotherapy.4cf0ed28.svg"
+  },
+  {
+    "revision": "f69b847f8210cea709f49ec39dd47e4b",
+    "url": "/dashboard-app/static/media/run.f69b847f.svg"
+  },
+  {
+    "revision": "02f11d1f91bda442b86a5763de986296",
+    "url": "/dashboard-app/static/media/sunny.02f11d1f.svg"
+  },
+  {
+    "revision": "a0b6279727ebdfa53a728423cf6aab2e",
+    "url": "/dashboard-app/static/media/weightlifting.a0b62797.svg"
+  }
+]);
